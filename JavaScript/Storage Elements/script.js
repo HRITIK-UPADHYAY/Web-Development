b5657ddec4e50747346c314c56b30df07fc183d1
HRITIK UPADHYAY <hritikupadhyay134@gamil.com>
2023-06-29 11:51:31 +0530

    Storage Elements

---
+++ JavaScript/Storage Elements/script.js
@@ -0,0 +1,14 @@
+/*
+Storage Elements -> are usedd to share the data accross multiply webpages of a website.
+
+1. Local Storage is pemanent.
+{key: value} => key and value need to be string datatype.
+localStorage.setItem("key", "value") // used for creating and updating.
+localStorage.getItem("key", "value") // this function returns the value of "key".
+localStorage.removeItem("key", "value") // remove the items with key =`key`.
+
+
+2. Sessions Storage much like LocalStorage and but it will be erased upon closing the website/browser.
+
+3. Cookie Storage
+*/
